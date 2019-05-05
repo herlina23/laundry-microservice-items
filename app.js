47b7aab3064737cc6d8fb864c9f1bb986fed0e5d
daffaputradamar <daffaputradamar@gmail.com>
2019-05-05 10:03:14 +0700

    fix automation in item in and out

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.use("/api/v1/itemouts", require("./routes/itemouts"));
 app.use("/api/v1/items", require("./routes/items"));
 app.use("/api/v1/outcomes", require("./routes/outcomes"));
 app.use("/api/v1/outcomeins", require("./routes/outcomeins"));
-app.use("/api/v1/mreports", require("./routes/mreports"));
-app.use("/api/v1/dreports", require("./routes/dreports"));
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, console.log(`Server started on Port ${PORT}`));
