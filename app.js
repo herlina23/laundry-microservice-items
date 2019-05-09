832b0560d1a36d3f878ca7ed99b1d8228212256d
herlina23 <frois.herlina@gmail.com>
2019-05-09 16:32:27 +0700

    remove typo

--- app.js
+++ app.js
@@ -18,11 +18,8 @@ app.use("/api/v1/itemouts", require("./routes/itemouts"));
 app.use("/api/v1/items", require("./routes/items"));
 app.use("/api/v1/outcomes", require("./routes/outcomes"));
 app.use("/api/v1/outcomeins", require("./routes/outcomeins"));
-<<<<<<< HEAD
 // app.use("/api/v1/mreports", require("./routes/mreports"));
 // app.use("/api/v1/dreports", require("./routes/dreports"));
-=======
->>>>>>> 47b7aab3064737cc6d8fb864c9f1bb986fed0e5d
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, console.log(`Server started on Port ${PORT}`));
